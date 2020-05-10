5a248e9177681b527e43dbe92a35f6bb305a2e84
VasylievHennadii <vga.kha@gmail.com>
2020-05-10 21:48:46 +0300

    template transfer. Part2

--- config/web.php
+++ config/web.php
@@ -8,6 +8,9 @@ $config = [
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
     'layout' => 'blog',
+    'name' => 'Classic Start Page',
+    'language' => 'ru',
+
     'aliases' => [
         '@bower' => '@vendor/bower-asset',
         '@npm'   => '@vendor/npm-asset',
