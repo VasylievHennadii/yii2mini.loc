9851a2899074ad0236cb5baaf67d12320025f848
VasylievHennadii <vga.kha@gmail.com>
2020-05-10 21:20:53 +0300

    template transfer. part1

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -125,4 +125,15 @@ class SiteController extends Controller
     {
         return $this->render('about');
     }
+    
+
+    /**
+     * Displays test page.
+     *
+     * @return string
+     */
+    public function actionTest()
+    {
+        return $this->render('test');
+    }
 }
